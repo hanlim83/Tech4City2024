8306a155a3dbdf85252da34b44bbdfe6c5e80fbb
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-07-11 02:11:03 +0800

    Baseline Backend (#1)
    * Initial AI Model Files
    
    * Initial AI Model Files
    
    * set up backend and gitignore
    
    * Add Model.pt
    
    * Update dependabot.yml
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by pyment
    
    * Restyled by reorder-python-imports
    
    * Restyled by whitespace
    
    ---------
    
    Co-authored-by: Juay Kai Xun <juaykaixun@hotmail.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    """ """
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    """
+
+    :param item_id: int:
+    :param q: str:  (Default value = None)
+
+    """
+    return {"item_id": item_id, "q": q}
---
+++ backend/model.py
@@ -0,0 +1,46 @@
+import torch
+from PIL import Image
+from PIL import ImageDraw
+
+model_path = "model.pt"
+
+
+def load_model(model_path):
+    """Load the model from a given path.
+
+    :param model_path: str
+    :returns: - model: The loaded YOLOv8 model.
+
+    """
+    model = torch.hub.load("ultralytics/yolov5", "custom", path=model_path)
+    return model
+
+
+def make_prediction(model, image_path):
+    """Make a prediction on a single image using the loaded model.
+
+    :param model: The loaded YOLOv8 model
+    :param image_path: str
+    :returns: - results: The prediction results.
+
+    """
+    img = Image.open(image_path)
+    results = model(img)
+    return results
+
+
+def draw_prediction_bb(image_path, results):
+    """Draw bounding boxes on the image based on the prediction results.
+
+    :param image_path: str
+    :param results: The prediction results from the model
+    :returns: - img: The image with bounding boxes drawn.
+
+    """
+    img = Image.open(image_path)
+    draw = ImageDraw.Draw(img)
+    for *xyxy, conf, cls in results.xyxy[0]:
+        label = results.names[int(cls)]
+        draw.rectangle(xyxy, outline=(255, 0, 0), width=2)
+        draw.text((xyxy[0], xyxy[1]), f"{label} {conf:.2f}", fill=(255, 0, 0))
+    return img
