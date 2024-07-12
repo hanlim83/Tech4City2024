da9ca342ace0f5f77e3e042f275f5fd68683f1ea
Restyled.io <commits@restyled.io>
2024-07-12 13:40:49 +0000

    Restyled by reorder-python-imports

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
 import model
 import uvicorn
-from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi import FastAPI
+from fastapi import File
+from fastapi import HTTPException
+from fastapi import UploadFile
 from pydantic import BaseModel
-from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy import Column
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///image_recognition.db"
 Base = declarative_base()
