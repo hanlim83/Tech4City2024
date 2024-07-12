15d8729a09f317cdce7ba89ed93c9fb4c70f3f3f
Juay Kai Xun <juaykaixun@hotmail.com>
2024-07-12 14:51:37 +0800

    init db

--- backend/database.py
+++ backend/database.py
@@ -9,12 +9,22 @@ def get_db():
 def create_tables():
     conn = get_db()
     cursor = conn.cursor()
+    ### uploads table ###
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS images (
+    CREATE TABLE IF NOT EXISTS uploads(
+        id INTEGER PRIMARY KEY,
+        filename TEXT NOT NULL,
+        uploadPath TEXT NOT NULL
+    )
+    ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS results (
         id INTEGER PRIMARY KEY,
         filename TEXT NOT NULL,
         label TEXT,
-        recognition_result TEXT
+        recognition_result TEXT,
+        upload_id INTEGER,
+        FOREIGN KEY (upload_id) REFERENCES uploads (id)
     )
     ''')
     conn.commit()
