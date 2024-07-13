04a438b04f7d382cb8a2f223dd950e912aaf2368
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-07-14 00:31:44 +0800

    Model Update (#16)
    Signed-off-by: Lyney <30753183+hanlim83@users.noreply.github.com>

---
+++ backend/test.py
@@ -0,0 +1,12 @@
+import model
+
+model = model.load_model()
+results = model("./images/fire2_mp4-68_jpg.rf.8cba43ae545a7f06cd149426135dc6c9.jpg")
+for result in results:
+    print(result.boxes)
+    print(result.masks)
+    print(result.keypoints)
+    print(result.obb)
+    print(result.names)
+    print(result.probs)
+    print(result.save("annotated_images/fire2_mp4-68_jpg.rf.8cba43ae545a7f06cd149426135dc6c9.jpg"))
